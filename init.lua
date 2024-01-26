89cab62714cb0aaaf06ce6847b6fda69b21f3c6f
Marco Della Rocca <marcodellarocca@MacBook-Pro-di-Marco.local>
2024-01-27 00:04:40 +0100

    feat: added keymaps

--- init.lua
+++ init.lua
@@ -366,7 +366,7 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<C-p>', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
@@ -608,3 +608,4 @@ cmp.setup {
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 vim.keymap.set({ 'n' }, '<leader>pv', vim.cmd.Ex, { desc = 'Explorer' })
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { buffer = bufnr, desc = '[G]it [S]tatus' })
