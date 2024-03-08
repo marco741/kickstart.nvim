d3f06546708a76b78ae35e5399b4a17b3ed39455
Marco Della Rocca <marco741147@gmail.com>
2024-03-08 23:29:55 +0100

    Improved autocomplete. Added go snippets

--- init.lua
+++ init.lua
@@ -96,19 +96,72 @@ require('lazy').setup({
   },
 
   {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
 
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
+      cmp.setup({
+        experimental = { ghost_text = true },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered()
+        },
+        mapping = {
+          ["<Up>"] = cmp.mapping.select_prev_item(),
+          ["<Down>"] = cmp.mapping.select_next_item(),
+          ["<Left>"] = cmp.mapping.select_prev_item(),
+          ["<Right>"] = cmp.mapping.select_next_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(),
+            { "i", "c" }),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true
+          })
+        },
+        -- ordered by priority
+        sources = cmp.config.sources({
+          { name = "nvim_lsp",               keyword_length = 1 },
+          { name = "nvim_lsp_signature_help" },
+          { name = "luasnip" },
+          { name = "path" },
+          { name = "nvim_lua" }
+        }, {
+          { name = 'buffer' },
+        })
+      })
 
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(), -- Tab for selection (arrows needed for selecting past items)
+        sources = { { name = "buffer" } }
+      })
+
+      cmp.setup.cmdline({ ":" }, {
+        mapping = cmp.mapping.preset.cmdline(), -- Tab for selection (arrows needed for selecting past items)
+        sources = { { name = "cmdline" }, { name = "path" } }
+      })
+    end
+  },
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/cmp-nvim-lua",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-nvim-lsp-signature-help",
+  "hrsh7th/cmp-path",
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = false,
+    dependencies = { "saadparwaiz1/cmp_luasnip" },
+    config = function()
+      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/custom/snippets" })
+    end
   },
 
 
